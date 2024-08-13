f035c5b8c030b71b2c45da91603dea6ffea055cd
meghagupta08 <guptamegha.0831@gmail.com>
2024-08-13 15:30:52 -0400

    spring starter kit

---
+++ fullStack/src/main/java/spring/practice/fullStack/PhotographyApplication.java
@@ -0,0 +1,13 @@
+package spring.practice.fullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotographyApplication.class, args);
+	}
+
+}
---
+++ fullStack/src/test/java/spring/practice/fullStack/PhotographyApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.practice.fullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotographyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
